123c2b0e857238cde45a65e15a396394fbccb7d3
eduardo jurado <eduardojurado986@gmail.com>
2026-03-20 17:21:22 -0600

    cambios en las rutas

--- src/routes/home.js
+++ src/routes/home.js
@@ -8,37 +8,36 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "../views/historia.html"));
+     res.sendFile(path.resolve(process.cwd(), "views/historia.html"));
 }) 
 
 router.get('/blog', (req, res) => {
-    res.sendFile(path.join(__dirname, "../views/blog.html"));
+    res.sendFile(path.resolve(process.cwd(), "views/blog.html"));
 
 })
 
 router.get('/beneficios', (req, res) => {
-    res.sendFile(path.join(__dirname, "../views/beneficios.html"));
+    res.sendFile(path.resolve(process.cwd(), "views/beneficios.html"));
 
 })
 
 router.get('/objetivos', (req, res) => {
-    res.sendFile(path.join(__dirname, "../views/objetivos.html"));
+   res.sendFile(path.resolve(process.cwd(), "views/objetivos.html"));
 
 })
 
 router.get('/paises', (req, res) => {
-    res.sendFile(path.join(__dirname, "../views/paises.html"));
+    res.sendFile(path.resolve(process.cwd(), "views/paises.html"));
 
 })
 
 router.get('/representantes', (req, res) => {
-    res.sendFile(path.join(__dirname, "../views/representantes.html"));
+    res.sendFile(path.resolve(process.cwd(), "views/representantes.html"));
 
 })
 
 router.get('/tienda', (req, res) => {
-    res.sendFile(path.join(__dirname, "../views/tienda.html"));
-
+    res.sendFile(path.resolve(process.cwd(), "views/tienda.html"));
 })
 
 export default router
