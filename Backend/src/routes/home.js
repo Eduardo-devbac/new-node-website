5b53678259c2e5a1fe2fe0743badb465e8f65dfc
eduardo jurado <eduardojurado986@gmail.com>
2026-03-22 22:21:05 -0600

    correcion de rutas

--- Backend/src/routes/home.js
+++ Backend/src/routes/home.js
@@ -8,36 +8,36 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 router.get('/', (req, res) => {
-     res.sendFile(path.join(process.cwd(), "../frontend/views-frontend/historia.html"));
+     res.sendFile(path.join(process.cwd(), "../frontend/pages/historia.html"));
 }) 
 
 router.get('/blog', (req, res) => {
-    res.sendFile(path.resolve(process.cwd(), "../frontend/views-frontend/blog.html"));
+    res.sendFile(path.resolve(process.cwd(), "../frontend/pages/blog.html"));
 
 })
 
 router.get('/beneficios', (req, res) => {
-    res.sendFile(path.resolve(process.cwd(), "../frontend/views-frontend/beneficios.html"));
+    res.sendFile(path.resolve(process.cwd(), "../frontend/pages/beneficios.html"));
 
 })
 
 router.get('/objetivos', (req, res) => {
-   res.sendFile(path.resolve(process.cwd(), "../frontend/views-frontend/objetivos.html"));
+   res.sendFile(path.resolve(process.cwd(), "../frontend/pages/objetivos.html"));
 
 })
 
 router.get('/paises', (req, res) => {
-    res.sendFile(path.resolve(process.cwd(), "../frontend/views-frontend/paises.html"));
+    res.sendFile(path.resolve(process.cwd(), "../frontend/pages/paises.html"));
 
 })
 
 router.get('/representantes', (req, res) => {
-    res.sendFile(path.resolve(process.cwd(), "../frontend/views-frontend/representantes.html"));
+    res.sendFile(path.resolve(process.cwd(), "../frontend/pages/representantes.html"));
 
 })
 
 router.get('/tienda', (req, res) => {
-    res.sendFile(path.resolve(process.cwd(), "../frontend/views-frontend/tienda.html"));
+    res.sendFile(path.resolve(process.cwd(), "../frontend/pages/tienda.html"));
 })
 
 export default router
