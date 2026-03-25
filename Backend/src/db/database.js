ac74ea1d7bf8d3337afb3247f9a99898f0c0269f
eduardo jurado <eduardojurado986@gmail.com>
2026-03-25 11:49:12 -0600

    cambio de la coneccion de db

--- Backend/src/db/database.js
+++ Backend/src/db/database.js
@@ -9,9 +9,9 @@ export const pool = mysql.createPool({
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
   port: process.env.MYSQLPORT,
-  ssl: {
+  /* ssl: {
     rejectUnauthorized: false
-  }
+  } */
 });
 pool.getConnection((err, connection) => {
     if (err) {
